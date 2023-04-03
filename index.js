9150eaec8158d3f67983f56f27f2bade3ce3a1ab
Munawar <munawerali1993@gmail.com>
2023-04-03 18:16:26 +0200

    Bugfix Navbar

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// import NavbarToggle from "./navbarToggle.js";
 import Nav from "./scripts/navbarToggle.js";
-import Alert from "./scripts/alertBox.js";
-import "./scripts/alertBox.js";
 
 const xs = matchMedia("(min-width:0px)");
 const sm = matchMedia("(min-width:480px)");
@@ -11,27 +8,3 @@ const xl = matchMedia("(min-width:1200px)");
 
 const nav = new Nav();
 nav.init();
-
-const alert = new Alert();
-alert.init();
-
-// const media = () => {
-//   if (xl.matches) {
-//     console.log("XL SIZE");
-//     document.querySelector(".media-query").style.width = "1000px";
-//   } else if (lg.matches) {
-//     console.log("LG SIZE");
-//     document.querySelector(".media-query").style.width = "800px";
-//   } else if (md.matches) {
-//     console.log("MD SIZE");
-//     document.querySelector(".media-query").style.width = "600px";
-//   } else if (sm.matches) {
-//     console.log("SM SIZE");
-//     document.querySelector(".media-query").style.width = "400px";
-//   } else if (xs.matches) {
-//     console.log("XS SIZE");
-//     document.querySelector(".media-query").style.width = "200px";
-//   }
-// };
-
-// const alertToggler = new AlertToggler();
--- scripts/navbarToggle.js
+++ scripts/navbarToggle.js
@@ -1,50 +1,33 @@
 class NavList {
   constructor(element) {
     this.element = element;
-    this.navItems = this.element.querySelectorAll(".nav-item");
-    this.openHeight = this.navItems.length * 45;
-    this.closeStyle =
-      "\
-      height:0px; \
-      margin-top:0; \
-    ";
-    this.openStyle =
-      "\
-      height:" +
-      this.openHeight +
-      "px; \
-      margin-top:1rem; \
-    ";
+    this.lg = matchMedia("(min-width:960px)");
+    window.addEventListener("resize", this.remove.bind(this));
     this.isOpen = false;
   }
 
   toggle() {
     if (this.isOpen) {
-      this.close();
+      this.element.classList.remove("open");
     } else {
-      this.open();
+      this.element.classList.add("open");
     }
+    this.isOpen = !this.isOpen;
   }
 
-  open() {
-    this.element.style.cssText = this.openStyle;
-    this.isOpen = true;
-    console.log(this.isOpen);
-  }
-
-  close() {
-    this.element.style.cssText = this.closeStyle;
-    this.isOpen = false;
-    console.log(this.isOpen);
+  remove() {
+    if (this.lg.matches) {
+      this.element.classList.remove("open");
+      this.isOpen = false;
+    }
   }
 }
 
 export default class Nav {
   constructor() {
     this.navLists = document.querySelectorAll(".nav-list");
-    this.xs = matchMedia("(min-width:0px)");
-    this.lg = matchMedia("(min-width:960px)");
     this.toggleBtns = document.querySelectorAll("nav label.toggle-btn");
+    this.xs = matchMedia("(min-width:0px)");
     this.navLists.forEach((navList) => {
       const name = navList.getAttribute("name");
       this[name] = new NavList(navList);
@@ -63,25 +46,9 @@ export default class Nav {
     this.toggleNav(labelFor);
   }
 
-  handleResize() {
-    this.navLists.forEach((navList) => {
-      if (this.lg.matches) {
-        navList.style.height = "fit-content";
-        navList.style.marginTop = "0";
-      } else if (this.xs.matches) {
-        navList.style.height = "0px";
-      }
-    });
-  }
-
   init() {
     this.toggleBtns.forEach((toggleBtn) => {
       toggleBtn.addEventListener("click", this.handleToggleNavClick.bind(this));
     });
-    this.handleResize();
-    window.addEventListener("resize", this.handleResize.bind(this));
   }
 }
-
-// const nav = new Nav();
-// nav.init();
